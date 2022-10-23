b5de7fc20faa0990b529b4d6812857dbfcebf448
Abhishek_Biswas <abiswah.2000@gmail.com>
2022-10-23 18:44:28 +0530

    code updated

--- docker.sh
+++ docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+#To download the given docker image
 echo "Downloading the stated docker image: $(docker pull brentley/ecsdemo-nodejs)"
 
-if [ $(echo $?) == 1 ]; then
-        echo "Running a docker container with the given image: $(docker run -itd --name ecsdemo-nodejs brentley/ecsdemo-nodejs)"
+#To launch a container from the downloaded image
+echo "Running a docker container with the given image: $(docker run -itd --name ecsdemo-nodejs brentley/ecsdemo-nodejs)"
 
-fi
 
 echo "Docker setup is done!"
