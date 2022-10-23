f22d2b6d89612bbdc6135f3b87d75c5c8a5d9c5b
Abhishek_Biswas <abiswah.2000@gmail.com>
2022-10-23 16:52:41 +0530

    code updated

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Downloading the stated docker image: $(sudo docker pull brentley/ecsdemo-nodejs)"
+echo "Downloading the stated docker image: $(sudo -S docker pull brentley/ecsdemo-nodejs)"
 
 if [ $(echo $?) == 1 ]; then
-        echo "Running a docker container with the given image: $(sudo docker run -itd --name ecsdemo-nodejs brentley/ecsdemo-nodejs)"
+        echo "Running a docker container with the given image: $(sudo -S docker run -itd --name ecsdemo-nodejs brentley/ecsdemo-nodejs)"
 
 fi
 
